d0a8842d3c74fc327f02c61a9731f10bd66d8525
s.ditaranto <simodita99@gmail.com>
2024-03-04 12:49:28 +0100

    store.js per l'api creato

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,38 @@
 <script>
+// per importare axios (dopo essere stato installato)
+import axios from 'axios';
+
+// importo lo store
+import { store } from './store.js';
+
+export default {
+
+  data() {
+    return {
+      
+      // dichiaro lo store, utilizzabile nel mio componente
+      store,
+
+    }
+  },
+
+  // spazio di codice che viene eseguito appena l'applicazione viene lanciata
+  created() {
+
+    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+    .then(res => {
+      this.store.cards = res.data;
+      console.log(this.store)
+    })
+
+  },
+
+  components: {
+
+  },
+
+
+}
 
 </script>
 
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+
+    // questo oggetto è come se fosse un data() condiviso da tutti i componenti della mia app
+    cards: [],
+
+})
